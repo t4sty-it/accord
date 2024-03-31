3161d8009de7998ba6b55d3e8788d5d36e98902b
t4sty <federico.federico.ceriani@gmail.com>
2024-03-31 17:52:49 +0200

    Cleanup

--- src/behaviors/SteerBehavior.ts
+++ src/behaviors/SteerBehavior.ts
@@ -13,21 +13,13 @@ export class SteerBehavior extends Component {
   ){ super() }
 
   private u = new Vec3(0, 1, 0)
-  private t = new Vec3(0, 0, 1)
   
-
   update(_time: number): void {
     const t = this.torque()
     this.rb.body.applyTorque(t)
-    // if (Math.random() < 0.1) console.log(this.strafe())
-    // this.rb.body.applyForce(this.strafe())
   }
 
   private torque(): Vec3 {
     return this.u.scale(this.strength * this.input.value)
   }
-
-  // private strafe(): Vec3 {
-  //   return this.rb.body.quaternion.vmult(this.t).vsub(this.rb.body.velocity)
-  // }
 }
